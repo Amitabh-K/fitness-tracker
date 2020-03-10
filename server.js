3be577fff5d4e901ec93c16595479f583981aac4
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-03-10 17:04:27 -0400

    make it better

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://<user>:<password12>@ds123971.mlab.com:23971/heroku_6tnc3khl", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
