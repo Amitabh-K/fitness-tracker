c813ab0f1364264cdec94604a3fb5561a63e3176
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-03-10 22:46:34 -0400

    add seeder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require('morgan');
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -13,14 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password12@ds123971.mlab.com:23971/heroku_6tnc3khl", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
