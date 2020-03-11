ba1a3c39052cc20a5f75a1c98585d4fab32f1f2c
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-03-10 22:53:23 -0400

    add seeder

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password12@ds123971.
 });
 
 // routes
-app.use(require("./routes/apiRoutes.js"));
-app.use(require("./routes/htmlRoutes.js"));
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
